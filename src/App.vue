6a36344a784a20f9c52017c87e6f6d6c6135c003
niloufar <zaferaniniloufar@gmail.com>
2021-12-19 09:50:21 +0330

    Upload simple PWA project on Github

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <header-bar></header-bar>
+    <data-table></data-table>
+    <b-container>
+      <b-row class="mt-5">
+        <router-view />
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+import HeaderBar from "@/components/HeaderBar.vue";
+import DataTable from "./components/DataTable .vue";
+
+export default {
+  components: {
+    HeaderBar,
+    DataTable,
+  },
+};
+</script>
